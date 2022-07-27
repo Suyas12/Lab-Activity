2e7033c0454bf837b16b0fa03b7d8e381e95f2db
Suyas12 <109739678+Suyas12@users.noreply.github.com>
2022-07-27 17:23:42 +0530

    Add files via upload

---
+++ Positive_Negative.java
@@ -0,0 +1,16 @@
+class Positive_Negative
+    {
+    public static void main(String args[])
+    {
+    int a=0;
+    if (a>0){
+    System.out.println("The number is positive");
+    }
+    else if (a==0){
+    System.out.println("The number is zero");   
+    }
+    else{
+    System.out.println("The number is negative");
+    }
+    }
+    }
